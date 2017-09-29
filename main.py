5ddeadd1ac38b83bc822c3ab0b870eaaf6ca674d
James Taber <jmtaber129@gmail.com>
2017-09-29 17:47:17 -0500

    Location indexing and get reports within radius endpoints.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # [START imports]
 import endpoints
+from google.appengine.api import search
 from google.appengine.ext import ndb
 from protorpc import message_types
 from protorpc import messages
 from protorpc import remote
 # [END imports]
 
+INDEX_NAME = 'reportsearch'
+REPORT_ID = 'report_id'
+LOCATION = 'location'
 
 # [START messages]
 class SendReportRequest(messages.Message):
@@ -21,6 +25,16 @@ class RoadkillReportResponse(messages.Message):
     latitude = messages.FloatField(1)
     longitude = messages.FloatField(2)
     timestamp = messages.StringField(3)
+    report_id = messages.StringField(4)
+    
+class GetRadiusReportsRequest(messages.Message):
+    latitude = messages.FloatField(1)
+    longitude = messages.FloatField(2)
+    radius = messages.IntegerField(3)
+    limit = messages.IntegerField(4)
+
+class GetRadiusReportsResponse(messages.Message):
+    reports = messages.MessageField(RoadkillReportResponse, 1, repeated=True)
     
 ROADKILL_RESOURCE = endpoints.ResourceContainer(
     message_types.VoidMessage,
@@ -46,6 +60,15 @@ class RoadkillApi(remote.Service):
     def report_roadkill(self, request):
         report = RoadkillReport(latitude=request.latitude, longitude=request.longitude)
         report_id = report.put().urlsafe()
+        
+        geopoint = search.GeoPoint(request.latitude, request.longitude)
+        index_fields = [
+          search.AtomField(name=REPORT_ID, value=report_id),
+          search.GeoField(name=LOCATION, value=geopoint)
+        ]
+        doc = search.Document(doc_id=report_id, fields=index_fields)
+        search.Index(name=INDEX_NAME).put(doc)
+        
         return SendReportResponse(report_id=report_id)
 
     @endpoints.method(
@@ -59,6 +82,26 @@ class RoadkillApi(remote.Service):
         report = report_key.get()
         resp = RoadkillReportResponse(latitude=report.latitude, longitude=report.longitude, timestamp=str(report.timestamp))
         return resp
+        
+    @endpoints.method(
+        GetRadiusReportsRequest,
+        GetRadiusReportsResponse,
+        path='roadkill/radius',
+        http_method='GET',
+        name='roadkill_radius'
+    )
+    def get_roadkill_in_radius(self, request):
+      reports = []
+      query = "distance({}, geopoint({},{})) < {}".format(LOCATION, request.latitude, request.longitude, request.radius)
+      results = search.Index(INDEX_NAME).search(query)
+      for doc in results:
+        report_id = doc.doc_id
+        report_key = ndb.Key(urlsafe=report_id)
+        ndb_report = report_key.get()
+        report = RoadkillReportResponse(latitude=ndb_report.latitude, longitude=ndb_report.longitude, timestamp=str(ndb_report.timestamp))
+        reports.append(report)
+      resp = GetRadiusReportsResponse(reports=reports)
+      return resp
 # [END roadkill_api]
 
 
