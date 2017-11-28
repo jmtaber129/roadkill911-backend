ca208038ad63120959ae1504d806b58c9eff00c2
James Taber <jmtaber129@gmail.com>
2017-11-27 20:50:47 -0600

    Fixed indentation.

--- main.py
+++ main.py
@@ -23,67 +23,67 @@ UPDATE_RESOURCE = endpoints.ResourceContainer(
 # [START roadkill_api]
 @endpoints.api(name='roadkill', version='v1')
 class RoadkillApi(remote.Service):
-    def __init__(self):
-      self.roadkill_report_manager = ReportManager()
-      self.group_manager = ControlGroupManager()
+  def __init__(self):
+    self.roadkill_report_manager = ReportManager()
+    self.group_manager = ControlGroupManager()
 
-    @endpoints.method(
-        models.SendReportRequest,
-        models.SendReportResponse,
-        path='roadkill',
-        http_method='POST',
-        name='report_roadkill')
-    def report_roadkill(self, request):
-        return self.roadkill_report_manager.create_report(request)
+  @endpoints.method(
+    models.SendReportRequest,
+    models.SendReportResponse,
+    path='roadkill',
+    http_method='POST',
+    name='report_roadkill')
+  def report_roadkill(self, request):
+    return self.roadkill_report_manager.create_report(request)
 
-    @endpoints.method(
-        ROADKILL_RESOURCE,
-        models.RoadkillReportResponse,
-        path='roadkill/{report_id}',
-        http_method='GET',
-        name='roadkill')
-    def get_roadkill_report(self, request):
-        return self.roadkill_report_manager.get_report(request.report_id)
-        
-    @endpoints.method(
-        models.GetRadiusReportsRequest,
-        models.GetRadiusReportsResponse,
-        path='roadkill/radius',
-        http_method='GET',
-        name='roadkill_radius'
-    )
-    def get_roadkill_in_radius(self, request):
-      return self.roadkill_report_manager.get_report_in_radius(request)
+  @endpoints.method(
+    ROADKILL_RESOURCE,
+    models.RoadkillReportResponse,
+    path='roadkill/{report_id}',
+    http_method='GET',
+    name='roadkill')
+  def get_roadkill_report(self, request):
+    return self.roadkill_report_manager.get_report(request.report_id)
       
-    @endpoints.method(
-        UPDATE_RESOURCE,
-        models.SendReportResponse,
-        path='roadkill/{report_id}',
-        http_method='PUT',
-        name='update_roadkill'
-    )
-    def update_roadkill_report(self, request):
-      return self.roadkill_report_manager.update_report(request, request.report_id)
-      
-    @endpoints.method(
-        models.CreateControlGroupRequest,
-        models.CreateControlGroupResponse,
-        path='control_group',
-        http_method='POST',
-        name='create_control_group'
-    )
-    def create_control_group(self, request):
-      return self.group_manager.create_group(request)
-      
-    @endpoints.method(
-        models.GetNearbyGroupsRequest,
-        models.GetNearbyGroupsResponse,
-        path='control_group',
-        http_method='GET',
-        name='nearby_control_groups'
-    )
-    def get_nearby_groups(self, request):
-      return self.group_manager.get_nearby_groups(request)
+  @endpoints.method(
+    models.GetRadiusReportsRequest,
+    models.GetRadiusReportsResponse,
+    path='roadkill/radius',
+    http_method='GET',
+    name='roadkill_radius'
+  )
+  def get_roadkill_in_radius(self, request):
+    return self.roadkill_report_manager.get_report_in_radius(request)
+    
+  @endpoints.method(
+    UPDATE_RESOURCE,
+    models.SendReportResponse,
+    path='roadkill/{report_id}',
+    http_method='PUT',
+    name='update_roadkill'
+  )
+  def update_roadkill_report(self, request):
+  return self.roadkill_report_manager.update_report(request, request.report_id)
+    
+  @endpoints.method(
+    models.CreateControlGroupRequest,
+    models.CreateControlGroupResponse,
+    path='control_group',
+    http_method='POST',
+    name='create_control_group'
+  )
+  def create_control_group(self, request):
+    return self.group_manager.create_group(request)
+    
+  @endpoints.method(
+    models.GetNearbyGroupsRequest,
+    models.GetNearbyGroupsResponse,
+    path='control_group',
+    http_method='GET',
+    name='nearby_control_groups'
+  )
+  def get_nearby_groups(self, request):
+    return self.group_manager.get_nearby_groups(request)
 # [END roadkill_api]
 
 
--- models.py
+++ models.py
@@ -10,62 +10,61 @@ METERS_PER_MILE = 1609.34
 
 # Proto messages.
 class ReportStatus(messages.Enum):
-    OPEN = 0
-    IN_PROGRESS = 1
-    CLOSED = 2
+  OPEN = 0
+  IN_PROGRESS = 1
+  CLOSED = 2
     
 class SendReportRequest(messages.Message):
-    latitude = messages.FloatField(1)
-    longitude = messages.FloatField(2)
-    status = messages.EnumField(ReportStatus, 3)
+  latitude = messages.FloatField(1)
+  longitude = messages.FloatField(2)
+  status = messages.EnumField(ReportStatus, 3)
 
 class SendReportResponse(messages.Message):
-    report_id = messages.StringField(1)
+  report_id = messages.StringField(1)
     
 class RoadkillReportResponse(messages.Message):
-    latitude = messages.FloatField(1)
-    longitude = messages.FloatField(2)
-    timestamp = messages.StringField(3)
-    status = messages.EnumField(ReportStatus, 4)
-    report_id = messages.StringField(5)
+  latitude = messages.FloatField(1)
+  longitude = messages.FloatField(2)
+  timestamp = messages.StringField(3)
+  status = messages.EnumField(ReportStatus, 4)
+  report_id = messages.StringField(5)
     
 class GetRadiusReportsRequest(messages.Message):
-    latitude = messages.FloatField(1)
-    longitude = messages.FloatField(2)
-    radius = messages.IntegerField(3)
-    limit = messages.IntegerField(4)
+  latitude = messages.FloatField(1)
+  longitude = messages.FloatField(2)
+  radius = messages.IntegerField(3)
+  limit = messages.IntegerField(4)
 
 class GetRadiusReportsResponse(messages.Message):
-    reports = messages.MessageField(RoadkillReportResponse, 1, repeated=True)
+  reports = messages.MessageField(RoadkillReportResponse, 1, repeated=True)
     
 class CreateControlGroupRequest(messages.Message):
-    name = messages.StringField(1)
-    email = messages.StringField(2)
-    latitude = messages.FloatField(3)
-    longitude = messages.FloatField(4)
-    radius = messages.FloatField(5)
+  name = messages.StringField(1)
+  email = messages.StringField(2)
+  latitude = messages.FloatField(3)
+  longitude = messages.FloatField(4)
+  radius = messages.FloatField(5)
     
 class CreateControlGroupResponse(messages.Message):
-    group_id = messages.StringField(1)
+  group_id = messages.StringField(1)
     
 class GetNearbyGroupsRequest(messages.Message):
-    latitude = messages.FloatField(1)
-    longitude = messages.FloatField(2)
+  latitude = messages.FloatField(1)
+  longitude = messages.FloatField(2)
         
 class GetNearbyGroupsResponse(messages.Message):
-    groups = messages.MessageField(CreateControlGroupRequest, 1, repeated=True)
+  groups = messages.MessageField(CreateControlGroupRequest, 1, repeated=True)
 
 # NDB models.
 class RoadkillReport(ndb.Model):
-    latitude=ndb.FloatProperty()
-    longitude=ndb.FloatProperty()
-    timestamp=ndb.DateTimeProperty(auto_now_add=True)
-    status=msgprop.EnumProperty(ReportStatus)
+  latitude=ndb.FloatProperty()
+  longitude=ndb.FloatProperty()
+  timestamp=ndb.DateTimeProperty(auto_now_add=True)
+  status=msgprop.EnumProperty(ReportStatus)
     
 class ControlGroup(ndb.Model):
-    name=ndb.StringProperty()
-    email=ndb.StringProperty()
-    latitude=ndb.FloatProperty()
-    longitude=ndb.FloatProperty()
-    radius=ndb.FloatProperty()
-
+  name=ndb.StringProperty()
+  email=ndb.StringProperty()
+  latitude=ndb.FloatProperty()
+  longitude=ndb.FloatProperty()
+  radius=ndb.FloatProperty()
